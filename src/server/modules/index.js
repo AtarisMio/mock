989bb1483d509152f1f5325d717881cea59aafad
xuhang <xuhang@gomeholdings.com>
2017-05-25 19:08:02 +0800

    fix

--- src/server/modules/index.js
+++ src/server/modules/index.js
@@ -1,22 +1,25 @@
-const Api = require('./api');
-const postValid = require('./postValid');
-const preValid = require('./preValid');
-const valid = require('./valid');
+const glob = require('glob');
+
+const modulesName = glob.sync('!(index.js)', {
+    cwd: __dirname
+})
+
+const modules = {};
+
+modulesName.map(moduleName =>
+    modules[moduleName.replace('.js', '')] = require(`./${moduleName}`)
+);
 
 function createTables(...tables) {
     for(let table of tables) {
-        table.sync(); // 不会drop掉已有的表 
+        table.sync({ force: true }); // 会drop掉已有的表 
     }
 }
 
-createTables(Api, preValid, postValid, valid);
+createTables(...Object.values(modules));
 
-module.exports = {
+module.exports = Object.assign({}, modules, {
     method: {
         createTables
-    },
-    Api,
-    preValid,
-    postValid,
-    valid
-};
+    }
+});
