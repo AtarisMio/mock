209fcc761c7b544ce3a1a7b1b28ebd0bdd62bfa1
xuhang <xuhang@gomeholdings.com>
2017-06-26 18:51:20 +0800

    完成接口

--- src/server/routers/api/v1/user.js
+++ src/server/routers/api/v1/user.js
@@ -3,25 +3,23 @@ const router = require('express').Router();
 const auth = require('./../../midwares/auth');
 const Packing = require('./../../../../utils/packing');
 
-const { userNameIsUsed,
+const { getUserInstanceByUsername,
+        userNameIsUsed,
         createUser,
+        signin,
         createToken,
         destroyToken } = require('./../../functions/user');
 
 router
     .post('/signin', auth(), async (req, res) => {
         const { username, password } = req.body;
-        // todo 验证用户信息
-
-        const authed = true;
-        // 根据认证结果
-        if (authed) {
+        // 验证用户信息 todo 接入 ldap
+        if (await signin(username, password)) {
             // todo 在库中查找当前用户 或 创建用户
             // await user.findOrCreate({});
-            // todo 生成token
-
-            const token = 'asd12e';
-            res.cookie('mockToken', token, {
+            const userInstance = await getUserInstanceByUsername(username);
+            // 生成token
+            res.cookie('mockToken', await createToken(userInstance), {
                 path: '/mock',
                 expires: new Date(Date.now() + config.cookieExpires)
             })
@@ -30,7 +28,7 @@ router
         } else {
             res.status(401).json(Packing({}, 401, 'Unauthorized', '认证失败 - 用户名或密码错误'));
         }
-    })
+    }) // done
     .get('/signout', auth(auth.user), async (req, res) => {
         if (req.cookies.mockToken) {
             const mockToken = req.cookies.mockToken;
@@ -41,7 +39,7 @@ router
                 .status(200)
                 .json(Packing({}, 0, 'Signout', '已安全退出'));
         }
-    })
+    }) // done
     .get('/check/username', auth(), async (req, res) => {
         const { username } = req.query;
         if (await userNameIsUsed(username)) {
@@ -57,7 +55,7 @@ router
             .json(Packing({
                 hasUsed: false
             }, 0, 'fresh username', '该用户名未注册'));
-    })
+    }) // done
     .post('/signup', auth(), async (req, res) => {
         const { username, password, confirmPassword } = req.body;
         if (password !== confirmPassword) {
@@ -74,7 +72,7 @@ router
                 }, 1, 'used username', '该用户名已被使用'));
             return;
         }
-        // todo 注册并登录
+
         const userInstance = await createUser(username, password);
 
         res
@@ -85,31 +83,46 @@ router
             .status(201)
             .json(Packing({}, 0, 'Authorized', '注册成功'));
     }) // done
-    .post('/set/:type', auth(auth.user), async (req, res) => {
-        switch (req.params.type) {
-            case 'chinesname':
-                // todo 设置用户中文名
-                break;
-
-            default:
-                break;
+    .patch('/', auth(auth.user), async (req, res) => {
+        const { password, confirmPassword, chineseName, apiToken } = req.body;
+        const user = req.userInfo;
+        if (password) {
+            if (password === confirmPassword) {
+                user.set({ password });
+            } else {
+                res
+                    .status(409)
+                    .json(Packing({}, 409, 'conflict password', '密码与确认密码不相同'));
+                return;
+            }
         }
-        res.send(200);
+        if (chineseName) {
+            user.set({ chineseName });
+        }
+        if (apiToken) {
+            user.set({ apiToken });
+        }
+        user.save();
+        const userInfo = user.get({ plain: true });
+        delete userInfo.password_hash;
+        delete userInfo.password;
+        delete userInfo.salt;
+        delete userInfo.id;
+
+        res.status(200).json(Packing({ userInfo }));
     })
     .get('/userInfo', auth(auth.user), async (req, res) => {
         // todo 查询用户信息
         const userInfo = req.userInfo.get({ plain: true });
-        const apis = await req.userInfo.getApi();
-        userInfo.apis = apis;
 
         delete userInfo.password_hash;
+        delete userInfo.password;
+        delete userInfo.salt;
         delete userInfo.id;
-        delete userInfo.createdAt;
-        delete userInfo.updatedAt;
 
         res
             .status(200)
-            .json(Packing(userInfo, 0, 'get userInfo', '用户信息'));
-    });
+            .json(Packing({ userInfo }, 0, 'get userInfo', '用户信息'));
+    }); // done
 
 module.exports = router;
