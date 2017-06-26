9e38c1ce975aa4559dbe4073a3c49953b6cb1f21
xuhang <xuhang@gomeholdings.com>
2017-06-26 18:48:21 +0800

    移出时态

--- src/mock/index.js
+++ src/mock/index.js
@@ -1,4 +1,22 @@
 const utils = require('./utils');
+const tense = require('./tense');
 
-module.exports = [utils.getUserInfo, utils.getApiRegex, utils.getPreValid, utils.getPostValid, utils.getPreDataGenerator, utils.getPostDataGenerator, // 预处理mock需要的规则
-    utils.onPreCapture, utils.onPreValid, utils.onPreDataGenerator, utils.onFetchData, utils.onPostDataGenerator, utils.onPostValid, utils.onPostCapture];
+module.exports = [
+    // start pretreatment 预处理mock需要的规则
+    utils.getUserInfo,
+    utils.getApiRegex,
+    utils.getPreValid,
+    utils.getPostValid,
+    utils.getPreDataGenerator,
+    utils.getPostDataGenerator,
+    // end pretreatment
+    // start treatment tense 处理数据时态
+    tense.onPreCapture,
+    tense.onPreValid,
+    tense.onPreDataGenerator,
+    tense.onFetchData,
+    tense.onPostDataGenerator,
+    tense.onPostValid,
+    tense.onPostCapture
+    // end treatment tense
+];
