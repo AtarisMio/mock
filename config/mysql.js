04b8c0b51dd871e6b1441b489c9f66814ae68083
zhangchenze <zhangchenze@gomeholdings.com>
2017-07-31 20:09:08 +0800

    add 使用文档

--- config/mysql.js
+++ config/mysql.js
@@ -1,7 +1,7 @@
 const development = {
     host        : 'localhost',
     user        : 'root',
-    password    : 'root',
+    password    : 'zhang@1993',
     database    : 'db_mock',
     tb_prefix   : 'mock',
     salt        : 'ace1302f35f06a21b3ef0af1e'
