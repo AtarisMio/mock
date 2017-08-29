bebeee9c26707010854b3e69af124dec6c5e6252
xuhang <xuhang@gomeholdings.com>
2017-08-29 18:50:55 +0800

    添加类型转换器

--- src/mock/gneratorMidware.js
+++ src/mock/gneratorMidware.js
@@ -4,9 +4,21 @@ const traverse = (data) => {
     for(let key in data) {
         if (typeof data[key] === 'object') {
             traverse(data[key]);
-        } else if (typeof data[key] === 'string' && /^\[Regex\].*?\[\/Regex\]$/.test(data[key])) {
-            const target = data[key].replace(/^\[Regex\](.*?)\[\/Regex\]$/, '$1');
-            data[key] = new DataGenerator(target).productData();
+        } else if (typeof data[key] === 'string' && /^\[Regex(?=[^\]]*\])\s*(type=("|'|)(number|string|boolean)\2|)\s*\].*?\[\/Regex\]$/.test(data[key])) {
+            const target = /^\[Regex(?=[^\]]*\])\s*(type=("|'|)(number|string|boolean)\2|)\s*\](.*?)\[\/Regex\]$/.exec(data[key]);
+            const dataType = target[3];
+            const targetData = target[4];
+            data[key] = new DataGenerator(targetData).productData();
+            switch(dataType) {
+                case 'number': {
+                    data[key] = Number(data[key]);
+                    break;
+                }
+                case 'boolean': {
+                    data[key] = data[key] === 'true' ? true : data[key] === 'false' ? false : null;
+                    break;
+                }
+            }
         }
     }
 }
